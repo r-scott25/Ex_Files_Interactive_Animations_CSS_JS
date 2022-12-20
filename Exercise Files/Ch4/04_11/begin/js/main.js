6fbe64bb45570cd4bd31f00e459312dfc31759dc
r-scott25 <rachelbernal2@gmail.com>
2022-12-20 16:13:50 -0600

    manipulating the DOM with JavaScript

--- Exercise Files/Ch4/04_11/begin/js/main.js
+++ Exercise Files/Ch4/04_11/begin/js/main.js
@@ -1,37 +1,48 @@
-window.addEventListener('DOMContentLoaded', function (e) {
-  var presentation = document.querySelector('hp-presentation');
+window.addEventListener("DOMContentLoaded", function (e) {
+  var presentation = document.querySelector("hp-presentation");
 
   presentation.onclick = handlePresentationClick;
-  presentation.addEventListener('animationend', handleAnimationEnd, false);
+  presentation.addEventListener("animationend", handleAnimationEnd, false);
 });
 
 function handlePresentationClick(e) {
-  var current = document.querySelector('hp-slide.active');
+  var current = document.querySelector("hp-slide.active");
   var next = current.nextElementSibling;
 
   if (next) {
-    current.classList.remove('active');
-    next.classList.add('active');
+    current.classList.remove("active");
+    next.classList.add("active");
 
-    next.querySelectorAll('.match').forEach(function (el) {
-      setTimeout(function () { el.classList.remove('match'); }, 0);
+    next.querySelectorAll(".match").forEach(function (el) {
+      setTimeout(function () {
+        el.classList.remove("match");
+      }, 0);
     });
 
-    var aa = parseInt(next.getAttribute('data-autoadvance'));
+    var aa = parseInt(next.getAttribute("data-autoadvance"));
 
     if (!isNaN(aa)) {
       setTimeout(function (e) {
         handlePresentationClick(e);
       }, aa);
     }
+
+    var osa = next.getAttribute("data-onshow");
+    if (osa) {
+      window[osa]();
+    }
   }
 }
 
 function handleAnimationEnd(e) {
-  var slide = e.target.closest('hp-slide');
-  var aa = slide.getAttribute('data-autoadvance');
+  var slide = e.target.closest("hp-slide");
+  var aa = slide.getAttribute("data-autoadvance");
 
-  if (aa == 'animationend' && slide.classList.contains('active')) {
+  if (aa == "animationend" && slide.classList.contains("active")) {
     handlePresentationClick(e);
   }
+}
+
+function runLearningSequence() {
+  console.log("in runLearningSequence");
 }
